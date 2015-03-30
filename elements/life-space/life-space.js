26ff350da61231da2eb8f91c08104486bd482349
thansber <thansber@yahoo.com>
2015-03-30 10:43:37 -0400

    player space handling is much improved
    - next space works

--- elements/life-space/life-space.js
+++ elements/life-space/life-space.js
@@ -2,6 +2,14 @@ Polymer({
 
   addInsurance: function(event, detail, sender) {
     this.player.insurance.push(sender.getAttribute('insurance'));
+    this.pay.call(this, arguments);
+  },
+
+  next: function(event, detail, sender) {
+    this.fire('next-space');
+  },
+
+  pay: function(event, detail, sender) {
     this.transaction(+sender.getAttribute('amount'));
   },
 
--- elements/life-spaces/life-spaces.js
+++ elements/life-spaces/life-spaces.js
@@ -1,6 +1,7 @@
 Polymer({
-  attached: function() {
-    this.spaces = this.shadowRoot.querySelectorAll('[space]');
+  nextSpace: function() {
+    this.$.spaces.selectNext(true);
+    this.player.space = this.$.spaces.selected;
   },
 
   observe: {
@@ -9,26 +10,18 @@ Polymer({
 
   playerChanged: function() {
     if (!this.player.space) {
-      this.player.space = this.spaces.item(0).id;
+      this.player.space = this.$.spaces.querySelectorAll('[space]').item(0).id;
     } else {
       this.spaceChanged();
     }
   },
 
   spaceChanged: function() {
-    var prevSpace = this.shadowRoot.querySelector('[space][active]'),
-        nextSpace;
-    if (prevSpace) {
-      prevSpace.removeAttribute('active');
-    }
-    nextSpace = this.$[this.player.space];
-    if (nextSpace) {
-      nextSpace.setAttribute('active', '');
-      this.style.top = this.topOffset();
-    }
+    this.$.spaces.selected = this.player.space;
+    this.style.top = this.topOffset();
   },
 
   topOffset: function() {
-    return (106 * this.player.index + 20) + 'px';
+    return (106 * this.player.index + 21) + 'px';
   }
 });
