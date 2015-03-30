93bec906c22543cb276a8456ef92e64a34935567
thansber <thansber@yahoo.com>
2015-03-30 13:10:52 -0400

    rename pay method to transfer

--- elements/life-space/life-space.js
+++ elements/life-space/life-space.js
@@ -2,20 +2,20 @@ Polymer({
 
   addInsurance: function(event, detail, sender) {
     this.player.insurance.push(sender.getAttribute('insurance'));
-    this.pay.apply(this, arguments);
+    this.transfer.apply(this, arguments);
   },
 
   next: function(event, detail, sender) {
     this.fire('next-space');
   },
 
-  pay: function(event, detail, sender) {
-    this.transaction(+sender.getAttribute('amount'));
-    this.next();
-  },
-
   transaction: function(amount) {
     this.player.cash += amount;
+  },
+
+  transfer: function(event, detail, sender) {
+    this.transaction(+sender.getAttribute('amount'));
+    this.next();
   }
 
 });
