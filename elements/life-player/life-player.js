f4b9ddc62cb9d7839889532c2aa0205f3bf5f575
thansber <thansber@yahoo.com>
2015-05-13 13:08:01 -0400

    use filter for cash, add animation when changing cash

--- elements/life-player/life-player.js
+++ elements/life-player/life-player.js
@@ -1,15 +1,47 @@
 Polymer({
 
-  cashChanged: function() {
-    this.player.formattedCash = this.formatCash();
+  animateCashChange: function(oldCash, newCash) {
+    var increment = Math.abs(oldCash - newCash) / 10;
+    this.animator = setInterval(function() {
+      if (this.player.cashChange === newCash) {
+        clearInterval(this.animator);
+        this.cashIsChanging(false);
+        return;
+      }
+      this.player.cashChange += increment * (oldCash < newCash ? 1 : -1);
+    }.bind(this), 50);
+  },
+
+  cashChanged: function(oldCash, newCash) {
+    if (!oldCash || !newCash) {
+      return;
+    }
+
+    clearInterval(this.animator);
+    this.$.changingCash.classList.toggle('adding', oldCash < newCash);
+    this.$.changingCash.classList.toggle('removing', oldCash > newCash);
+    this.player.cashChange = oldCash;
+    this.cashIsChanging(true);
+    this.async('animateCashChange', [oldCash, newCash]);
+  },
+
+  cashIsChanging: function(isChanging) {
+    if (isChanging) {
+      this.$.cash.setAttribute('hidden', '');
+      this.$.changingCash.removeAttribute('hidden');
+    } else {
+      this.$.cash.removeAttribute('hidden');
+      this.$.changingCash.setAttribute('hidden', '');
+    }
   },
 
   created: function() {
     this.cashFormatter = /(\d+)(\d{3})/;
+    this.changing = false;
   },
 
-  formatCash: function() {
-    var formattedCash = '' + this.player.cash;
+  formatCash: function(cash) {
+    var formattedCash = '' + (cash || '0');
     while (this.cashFormatter.test(formattedCash)) {
       formattedCash = formattedCash.replace(this.cashFormatter, '$1' + ',' + '$2');
     }
