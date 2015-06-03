7dda44574837adf7adfd047caae47c64cb5e8d66
thansber <thansber@yahoo.com>
2015-06-03 14:02:10 -0400

    first attempt at integrating vulcanize

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
+  connect = require('gulp-connect'),
+  vulcanize = require('gulp-vulcanize');
 
 gulp.task('connect', function() {
   connect.server();
+});
+
+gulp.task('vulcanize', function() {
+  return gulp.src('raw.html')
+    .pipe(vulcanize({
+    }))
+    .pipe(gulp.dest('./index'));
 });
 
 gulp.task('default', ['connect']);
